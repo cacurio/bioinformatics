6541db13cf7debf61db81a57e8b80f68edbb44ce
cacurio <alan.acurio@gmail.com>
2019-11-06 13:58:59 -0500

    refactor

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
 import os
+import re
+
+EXTENSION_FILE = '.fasta'
+
 
 # Crea un directorio
 def create_directory():
-    directory = "/Users/cristhian/Proyectos/basch/project_bio_info/sequences/"
+    path = os.path.dirname(os.path.realpath(__file__)) + "/sequences/"
     try:
-        os.stat(directory)
+        os.stat(path)
     except:
-        os.mkdir(directory)
-    return directory
+        os.mkdir(path)
+    return path
+
+
+# obtener el nombre del archivo
+def regex_head(head):
+    segment = re.search(r'>(\w*.\d*)', head)
+    return segment.group(1)
 
 
 # procesa el file .fasta
 def process_file(file):
     input = open(file)
+    directory = create_directory()
     for line in input:
         if line[0] == '>':
-            output = open(create_directory() + line + '_seq.fasta', 'w')
+            name_file = regex_head(line)
+            output = open(directory + name_file + EXTENSION_FILE, 'w')
             output.write(line)
         else:
             output.write(line)
 
     input.close()
     output.close()
+
 
 # arranca el programa
 if __name__ == '__main__':
